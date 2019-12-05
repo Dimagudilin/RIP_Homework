e03dedf9338e9d38af4713c91e9cc5d2be073c8d
Dimagudilin <dima.gudilin@mail.ru>
2019-12-06 00:29:30 +0300

    paginator

--- books/views.py
+++ books/views.py
@@ -1,17 +1,24 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Book
 
 
 
 
 def book_detail(request,ident):
+    # Получаем книгу по id
     book = get_object_or_404(Book, id=ident)
     return render(request, 'detail.html', {'book': book})
 
-def book_list(request):
-    # Отображение
-    books = Book.objects.all()
-    return render(request, 'list.html', {'books': books})
 
-
+class BookListView(ListView):
+    queryset = Book.objects.all()
+    # Контекстная переменная (на странице)
+    context_object_name = 'books'
+    # Количество книг на странице
+    paginate_by = 4
+    # Название шаблона
+    template_name = 'list.html'
 
